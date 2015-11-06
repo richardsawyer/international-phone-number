aa04a3e668a53bf6b73df9cf9458c9462aa4b94c
richardsawyer <richard@spinalogic.org>
2015-11-06 10:14:12 +1000

    Use E164 in model

--- releases/international-phone-number.js
+++ releases/international-phone-number.js
@@ -84,7 +84,7 @@
             if (!value) {
               return value;
             }
-            return value.replace(/[^\d]/g, '');
+            return element.intlTelInput('getNumber'); // value.replace(/[^\d]/g, '');
           });
           ctrl.$validators.internationalPhoneNumber = function(value) {
             var selectedCountry;
